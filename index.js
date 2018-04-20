4c07fbfa143d39188142cdf12d251668ebbe836e
terara <terahara_a@pc4592.mti.co.jp>
2018-04-20 11:48:03 +0900

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+/** @jsx h */
+import { h, app } from "hyperapp";
+
+const state = {
+  count: 0
+};
+
+const actions = {
+  down: () => state => ({ count: state.count - 1 }),
+  up: () => state => ({ count: state.count + 1 })
+};
+
+const view = (state, actions) =>
+  h("div", {}, [
+    h("h1", {}, state.count),
+    h("button", { onclick: () => actions.down(1) }, "-"),
+    h("button", { onclick: () => actions.up(1) }, "+")
+  ]);
+
+export const main = app(state, actions, view, document.body);
